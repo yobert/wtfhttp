7b8fb54c586a6c0889b2677fef7ca74d9b275b6c
Joel Jensen <yobert@gmail.com>
2021-09-17 13:48:13 -0600

    Improvements to color output, serialize mode, and request ID logging

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/yobert/wtfhttp
 
 go 1.14
+
+require (
+	github.com/gabriel-vasile/mimetype v1.3.1
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,6 +12,16 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
+	"sync/atomic"
+
+	"github.com/gabriel-vasile/mimetype"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+const (
+	maxbody   = 1024
+	serialize = false
 )
 
 func main() {
@@ -24,36 +35,79 @@ func main() {
 		target = os.Args[2]
 	}
 
-	log.Printf("Listening on %s, proxying requests to %s\n", listen, target)
+	colors := terminal.IsTerminal(int(os.Stdout.Fd()))
+
+	log.Printf("Listening on %s, proxying requests to %s, colors %#v, serialize %#v\n", listen, target, colors, serialize)
+
+	var nextid uint64
+
+	var lock sync.Mutex
 
 	proxy := &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
-			fmt.Println(BrightText(Magenta)+req.Proto, req.Method+Reset(), Text(Magenta)+req.URL.String()+Reset())
-			print_headers(req.Header, Magenta, map[string][]string{"Host": {req.Host}})
-			fmt.Println()
+			if serialize {
+				lock.Lock()
+			}
+
+			id := atomic.AddUint64(&nextid, 1)
+
+			o := &bytes.Buffer{}
+
+			if colors {
+				fmt.Fprintln(o, All(Black, false, Blue)+fmt.Sprintf(" %d ", id)+Reset(), BrightText(Blue)+req.Method+Reset(), Text(Blue)+req.URL.String()+Reset())
+			} else {
+				fmt.Fprintln(o, id, req.Method, req.URL)
+			}
+			print_headers(o, colors, req.Header, Blue, map[string][]string{"Host": {req.Host}})
+			fmt.Fprintln(o)
 
 			if req.Body != nil {
 				bodyBytes, err := ioutil.ReadAll(req.Body)
 				if err != nil {
 					log.Println(err)
 				} else {
+					print_mime(o, colors, Blue, bodyBytes)
 					req.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-					print_body(bodyBytes)
+					print_body(o, bodyBytes)
 				}
 			}
 
 			req.URL.Scheme = "http"
 			req.URL.Host = target
+
+			req.Header.Set("X-WTF-ID", fmt.Sprintf("%d", id))
+
+			fmt.Print(o.String())
 		},
 		ModifyResponse: func(res *http.Response) error {
-			fmt.Println(BrightText(Green)+res.Proto, Text(Green)+res.Status+Reset())
-			print_headers(res.Header, Green, map[string][]string{})
-			fmt.Println()
+			id, _ := strconv.Atoi(res.Request.Header.Get("X-WTF-ID"))
+
+			c := Green
+			if res.StatusCode >= 400 {
+				c = Red
+			}
+
+			o := &bytes.Buffer{}
+
+			if colors {
+				fmt.Fprintln(o, All(Black, false, c)+fmt.Sprintf(" %d ", id)+Reset(), BrightText(c)+res.Status+Reset())
+			} else {
+				fmt.Fprintln(o, id, res.Status)
+			}
+			print_headers(o, colors, res.Header, c, map[string][]string{})
+			fmt.Fprintln(o)
 
 			if res.Body != nil {
 				bodyBytes, _ := ioutil.ReadAll(res.Body)
+				print_mime(o, colors, c, bodyBytes)
 				res.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-				print_body(bodyBytes)
+				print_body(o, bodyBytes)
+			}
+
+			fmt.Print(o.String())
+
+			if serialize {
+				lock.Unlock()
 			}
 
 			return nil
@@ -63,7 +117,7 @@ func main() {
 	log.Fatal(http.ListenAndServe(listen, proxy))
 }
 
-func print_headers(headers http.Header, clr Color, extra map[string][]string) {
+func print_headers(dest io.Writer, colors bool, headers http.Header, clr Color, extra map[string][]string) {
 	keys := make([]string, 0, len(headers))
 	for k, _ := range headers {
 		keys = append(keys, k)
@@ -74,19 +128,44 @@ func print_headers(headers http.Header, clr Color, extra map[string][]string) {
 	sort.Strings(keys)
 	for _, k := range keys {
 		for _, v := range headers[k] {
-			fmt.Println(BrightText(clr)+k+Reset()+":", Text(clr)+v+Reset())
+			if colors {
+				fmt.Fprintln(dest, BrightText(clr)+k+Reset()+":", Text(clr)+v+Reset())
+			} else {
+				fmt.Fprintln(dest, k+":", v)
+			}
 		}
 		for _, v := range extra[k] {
-			fmt.Println(BrightText(clr)+k+Reset()+":", Text(clr)+v+Reset())
+			if colors {
+				fmt.Fprintln(dest, BrightText(clr)+k+Reset()+":", Text(clr)+v+Reset())
+			} else {
+				fmt.Fprintln(dest, k+":", v)
+			}
 		}
 	}
 }
 
-func print_body(buf []byte) {
+func print_mime(dest io.Writer, colors bool, clr Color, buf []byte) {
+	mtype := mimetype.Detect(buf)
+	if mtype.String() == "application/octet-stream" {
+		return
+	}
+	if colors {
+		fmt.Fprintln(dest, Text(clr)+mtype.String()+Reset())
+	} else {
+		fmt.Fprintln(dest, mtype)
+	}
+	fmt.Fprintln(dest)
+}
+
+func print_body(dest io.Writer, buf []byte) {
+	if len(buf) == 0 {
+		return
+	}
+
 	trimmed := 0
-	if len(buf) > 8192 {
-		trimmed = len(buf) - 8192
-		buf = buf[0:8192]
+	if len(buf) > maxbody {
+		trimmed = len(buf) - maxbody
+		buf = buf[0:maxbody]
 	}
 	escaped := string(buf)
 	escaped = strconv.Quote(escaped)
@@ -96,5 +175,6 @@ func print_body(buf []byte) {
 	if trimmed > 0 {
 		escaped += fmt.Sprintf(" (trimmed %d bytes)", trimmed)
 	}
-	fmt.Println(escaped)
+	fmt.Fprintln(dest, strings.TrimSpace(escaped))
+	fmt.Fprintln(dest)
 }
